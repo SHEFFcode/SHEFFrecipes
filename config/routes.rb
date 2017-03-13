b8593622c4b49ce6e72677cf9e3b453d276a56b2
SHEFFcode <sheff@sheffmachine.com>
2017-03-12 19:06:49 -0700

    add the like functionality to the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
