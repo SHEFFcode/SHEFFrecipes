ac00216263f9ccf586f1234b936fbf8807fd1740
SHEFFcode <sheff@sheffmachine.com>
2017-03-12 21:20:04 -0700

    set up the login for the user

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   resources :chefs, except: [:new]
 
   get '/register', to: 'chefs#new'
+
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
